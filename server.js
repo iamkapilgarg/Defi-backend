8eaffe31d6cb0df9e6276c07f5a283391f7f431b
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-07 13:15:39 -0700

    server setup

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Express = require('express');
 const App = Express();
 const BodyParser = require('body-parser');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const userRoutes = require('./routes/users');
 const projectRoutes = require('./routes/projects');
 const imageRoutes = require('./routes/images');
