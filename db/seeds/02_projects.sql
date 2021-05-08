b6f7479c448f91cf75ca082ca0550f74ff49ee47
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-08 14:11:07 -0700

    removing storing image logic

--- db/seeds/02_projects.sql
+++ db/seeds/02_projects.sql
@@ -1,4 +1,4 @@
-INSERT INTO projects (name, description, target_amount, target_date, min_amount, round, contract, user_id, link)
-VALUES('Mixickers', 'Reusable Cocktail Recipe Stickers', 10000, 'May 30, 2021', 10, 3, 'I will refund the money if the deadline is not met', 1, 'https://www.bbcgoodfood.com/recipes/collection/cocktail-recipes');
-INSERT INTO projects (name, description, target_amount, target_date, min_amount, round, contract, user_id, link)
-VALUES('Stella', 'An inspiring story about a foster husky', 20000, 'Jun 30, 2021', 10, 3, 'Once you invest, forget about your money. But trust me.', 2, 'https://dogtime.com/dog-breeds/siberian-husky#/slide/1');
+INSERT INTO projects (name, description, target_amount, target_date, min_amount, round, contract, user_id, link, image)
+VALUES('Mixickers', 'Reusable Cocktail Recipe Stickers', 10000, 'May 30, 2021', 10, 3, 'I will refund the money if the deadline is not met', 1, 'https://www.bbcgoodfood.com/recipes/collection/cocktail-recipes', 'https://defi-images.s3-us-west-2.amazonaws.com/cocktail.jpeg');
+INSERT INTO projects (name, description, target_amount, target_date, min_amount, round, contract, user_id, link, image)
+VALUES('Stella', 'An inspiring story about a foster husky', 20000, 'Jun 30, 2021', 10, 3, 'Once you invest, forget about your money. But trust me.', 2, 'https://dogtime.com/dog-breeds/siberian-husky#/slide/1', 'https://defi-images.s3-us-west-2.amazonaws.com/Husky.jpeg');
