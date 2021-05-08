08738318e1aef95d9f50936dc24eeae53ca03978
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-08 11:26:29 -0700

    adding form validations

--- routes/projects.js
+++ routes/projects.js
@@ -11,20 +11,20 @@ const multerS3 = require('multer-s3')
 router.get("/", (req, res) => {
   listProjects().then((projects) => {
     const promises = [];
-    for(let project of projects) {
+    for (let project of projects) {
       promises.push(getImagesByProjectsId(project.id));
     }
     Promise.all(promises).then((images) => {
       const projectsArray = [];
-      for(let image of images) {
-        for(let project of projects) {
-          if(image[0].project_id === project.id) {
+      for (let image of images) {
+        for (let project of projects) {
+          if (image[0].project_id === project.id) {
             console.log('inside a')
-            projectsArray.push({... project, "images": image})
+            projectsArray.push({ ...project, "images": image })
           }
         }
       }
-      res.status(200).json(projectsArray);
+      return res.status(200).json(projectsArray);
     });
   });
 });
@@ -34,8 +34,8 @@ router.get("/:id", (req, res) => {
   getProjectById(projectId).then((data) => {
     let project = {};
     getImagesByProjectsId(data[0].id).then((images) => {
-      project = {...data[0], images}
-      res.status(200).json(project);
+      project = { ...data[0], images }
+      return res.status(200).json(project);
     });
   });
 });
@@ -44,20 +44,20 @@ router.get("/users/:id", (req, res) => {
   const userId = req.params.id;
   getProjectsByUserId(userId).then((projects) => {
     const promises = [];
-    for(let project of projects) {
+    for (let project of projects) {
       promises.push(getImagesByProjectsId(project.id));
     }
     Promise.all(promises).then((images) => {
       const projectsArray = [];
-      for(let image of images) {
-        for(let project of projects) {
-          if(image[0].project_id === project.id) {
+      for (let image of images) {
+        for (let project of projects) {
+          if (image[0].project_id === project.id) {
             console.log('inside a')
-            projectsArray.push({... project, "images": image})
+            projectsArray.push({ ...project, "images": image })
           }
         }
       }
-      res.status(200).json(projectsArray);
+      return res.status(200).json(projectsArray);
     });
   });
 });
@@ -67,20 +67,20 @@ router.get("/users/fundings/:id", (req, res) => {
   getProjectsByInvestorId(userId).then((projects) => {
     console.log(projects)
     const promises = [];
-    for(let project of projects) {
+    for (let project of projects) {
       promises.push(getImagesByProjectsId(project.id));
     }
     Promise.all(promises).then((images) => {
       const projectsArray = [];
-      for(let image of images) {
-        for(let project of projects) {
-          if(image[0].project_id === project.id) {
+      for (let image of images) {
+        for (let project of projects) {
+          if (image[0].project_id === project.id) {
             console.log('inside a')
-            projectsArray.push({... project, "images": image})
+            projectsArray.push({ ...project, "images": image })
           }
         }
       }
-      res.status(200).json(projectsArray);
+      return res.status(200).json(projectsArray);
     });
   });
 });
@@ -103,10 +103,26 @@ const upload = multer({
   })
 });
 
-//use by upload form
 router.post('/', upload.array('image', 1), (req, res, next) => {
-  console.log(req.files)
   const body = req.body;
+  if (body.name === undefined || body.name.trim() === '') {
+    return res.status(400).json({ "message": "name cannot be blank" })
+  }
+  if (body.description === undefined || body.description.trim() === '') {
+    return res.status(400).json({ "message": "description cannot be blank" })
+  }
+  if (body.target_amount === undefined || body.target_amount === 0) {
+    return res.status(400).json({ "message": "target_amount cannot be blank or zero" })
+  }
+  if (body.min_amount === undefined || body.min_amount === 0) {
+    return res.status(400).json({ "message": "min_amount cannot be blank" })
+  }
+  if (body.target_date === undefined || body.target === '') {
+    return res.status(400).json({ "message": "target_date cannot be blank" })
+  }
+  if (body.contract === undefined || body.contract.trim() === '') {
+    return res.status(400).json({ "message": "contract cannot be blank" })
+  }
   const project = {
     name: body.name,
     description: body.description,
@@ -119,16 +135,17 @@ router.post('/', upload.array('image', 1), (req, res, next) => {
     link: body.link,
   }
   saveProject(project).then((data) => {
-    const image = {
-      project_id: data[0],
-      image: req.files[0].location
+    if (req.files.length > 0) {
+      const image = {
+        project_id: data[0],
+        image: req.files[0].location
+      }
+      saveImage(image).then((data) => {
+        console.log("image saved successfully")
+      });
     }
-    saveImage(image).then((data) => {
-      console.log("image saved successfully")
-    })
-    res.status(201).json({ "message": "project saved successfully", id: data[0] });
+    return res.status(201).json({ "message": "project saved successfully", id: data[0] });
   })
 });
-
 
 module.exports = router;
