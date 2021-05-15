6fc6cd4fdde7071d412caf6656da0e8ac231e630
kapildoppiogroup <iamkapilgarg@gmail.com>
2021-05-15 10:37:27 -0700

    adding username

--- db/queries/projects.js
+++ db/queries/projects.js
@@ -2,19 +2,22 @@ const knex = require('../../knexfile');
 
 const getProjectById = (id) => {
   return knex('projects')
-    .where('id', id)
-    .select('*');
+  .join('users', 'users.auth_id', 'projects.auth_id')
+    .where('projects.id', id)
+    .select('projects.*', 'users.name as username', 'users.id as user_id');
 }
 
 const listProjects = () => {
   return knex('projects')
-    .select('*');
+  .join('users', 'users.auth_id', 'projects.auth_id')
+    .select('projects.*', 'users.name as username', 'users.id as user_id');
 }
 
 const getProjectsByUserId = (id) => {
   return knex('projects')
+  .join('users', 'users.auth_id', 'projects.auth_id')
   .where('auth_id', id)
-  .select('*');
+  .select('projects.*', 'users.name as username', 'users.id as user_id');
 }
 
 const saveProject = (project) => {
